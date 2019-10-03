a08ae194a3edcc6e1b2c27734ec0ea110fe3669a
Satria Aji Putra <satmaxt@outlook.com>
2019-10-03 15:29:39 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118068/latihan18/kelipatan/PBO210118068Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Program untuk menghitung nilai kelipatan 3.5 dengan
+ * perulangan / looping for
+ */
+public class PBO210118068Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(float i = 3.5F; i <= 35; i+=3.5)
+            System.out.println(i);
+    }
+    
+}
